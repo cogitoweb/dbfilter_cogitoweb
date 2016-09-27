15c9b6055279c7ecc587c1742142c8141eac160a
2z <paolo.cazzitti@cogitoweb.it>
2016-09-27 18:01:16 +0200

    dbfilter

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+{
+	'name' : 'DbFilter',
+	'version' : '1.0',
+	'author' : "Paolo Cazzitti'",
+	'website' : 'http://www.cogitoweb.it/',
+	'category' : 'Tools',
+	'depends' : ['base'],
+	'description' : 'Enhanced DbFilter',
+	'data': [
+        'cogito_procedurericalcolo.xml',
+        'menu_items.xml',
+    ],
+
+	'installable': True
+}
---
+++ model/http.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Paolo Cazzitti
+#    Copyright 2016 Cogito Srl
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import http
+
+def db_filter(dbs, httprequest=None):
+    httprequest = httprequest or request.httprequest
+    h = httprequest.environ.get('HTTP_HOST', '').split(':')[0]
+    d, _, r = h.partition('.')
+    if d == "www" and r:
+        d = r.partition('.')[0]
+    
+    out_dbs = []
+    
+    if d == 'odoo':
+        out_dbs = ['cogitoweb'] 
+    elif d.isdigit():
+        out_dbs = dbs
+    else:
+        r = openerp.tools.config['dbfilter'].replace('%h', h).replace('%d', d)
+        out_dbs = [i for i in dbs if re.match(r, i)]
+        
+    return out_dbs
+
+http.db_filter = db_filter
