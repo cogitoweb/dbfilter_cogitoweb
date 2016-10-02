608888f7a2e2a1f3d8a2cf15932b0c50515401d9
Paolo Cazzitti <paolo.cazzitti@cogitoweb.it>
2016-10-02 11:46:07 +0200

    manage databases only on ip address

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dbfilter_cogitoweb
 odoo dbfilter
+
+this module has to be loaded on server startup
+In a buildout config append on section [odoo]
+    server_wide_modules = web,dbfilter_cogitoweb,...
--- __openerp__.py
+++ __openerp__.py
@@ -1,14 +1,14 @@
 {
 	'name' : 'DbFilter',
 	'version' : '1.0',
-	'author' : "Paolo Cazzitti",
+	'author' : "Cogito srl",
 	'website' : 'http://www.cogitoweb.it/',
 	'category' : 'Tools',
 	'depends' : ['base'],
 	'description' : 'Enhanced DbFilter',
 	'data': [
-    ],
-
+        	'views/webclient_templates.xml'
+    	],
 	'installable': True,
     'auto_install': True
 }
